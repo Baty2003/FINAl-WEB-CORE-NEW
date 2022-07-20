d673144395ce52bdece5b5fe5d603984b94f9c4b
Oleg Voskletsov <vosf03@gmail.com>
2022-07-20 08:15:10 +0500

    refactore code scss

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
+import { startSwiper } from './slider.js'
 import '../scss/style.scss'
 import {initLessMoreButton} from './showMore'
-import { startSwiper } from './slider.js'
 import {buttonListenerOpenMenus} from './openMobileMenus.js'
 
 
-initLessMoreButton('description', 8);
+initLessMoreButton('brends', 8);
 initLessMoreButton('repair-different-techniques', 3);
 startSwiper()
 buttonListenerOpenMenus();
